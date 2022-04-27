0f51064970e1c3a1d7f42868691d251b35afc027
Luis Ortiz <luiann.ortiz@gmail.com>
2022-04-26 23:40:40 -0500

    refactoring explorerService

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
-const fs = require("fs");
 
-// Part 1 Read json file ===========================
-const rawdata = fs.readFileSync("explorers.json");
-const explorers = JSON.parse(rawdata);
+const Reader = require('./lib/utils/Reader')
+const ExplorerService = require('./lib/services/ExplorerService')
 
-// Part 2: Get the quantity of explorers names in node
-const explorersInNode = explorers.filter((explorer) => explorer.mission == "node");
-//console.log(explorersInNode.length)
+const explorers = Reader.readJsonFile("explorers.json")
+//console.log(explorers)
 
-// Part4: Get the explorer's usernames in Node
-const explorersInNodeToGetUsernames = explorers.filter((explorer) => explorer.mission == "node");
-const usernamesInNode = explorersInNodeToGetUsernames.map((explorer) => explorer.githubUsername);
-//console.log(usernamesInNode)
+// Aplicación del ExplorerService sobre la lista de explorers
+const mission = "node"
+ExplorerService.filterByMission(explorers, mission);
+ExplorerService.getAmountOfExplorersByMission(explorers, mission);
+ExplorerService.getExplorersUsernamesByMission(explorers, mission);
+
+
 
 // DEAD CODE: Part 5,6,7, please remove this and go to Part 8!
 
