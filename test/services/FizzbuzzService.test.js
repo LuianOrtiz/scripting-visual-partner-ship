1e14223adfc0c0b7f22ff4fc6f7b8e560919312d
Luis Ortiz <luiann.ortiz@gmail.com>
2022-04-28 00:20:37 -0500

    Add test to FizzbuzzService

---
+++ test/services/FizzbuzzService.test.js
@@ -0,0 +1,32 @@
+const FizzbuzzService = require('./../../lib/services/FizzbuzzService')
+
+describe(`Test FizzbuzzService`, () => {
+
+    test(`1. Explorer trick equals to a number`, () => {
+        const explorer = {name: "Explorer1", score: 1}
+        const fbSerive = FizzbuzzService.applyValidationInExplorer(explorer)
+
+        expect(fbSerive.trick).toBe(1)
+    })
+
+    test(`2. Explorer trick equals to "Fizz"`, () => {
+        const explorer = {name: "Explorer2", score: "FIZZ"}
+        const fbSerive = FizzbuzzService.applyValidationInExplorer(explorer)
+
+        expect(fbSerive.trick).toBe("FIZZ")
+    })
+
+    test(`3. Explorer trick equals to "Buzz"`, () => {
+        const explorer = {name: "Explorer2", score: "BUZZ"}
+        const fbSerive = FizzbuzzService.applyValidationInExplorer(explorer)
+
+        expect(fbSerive.trick).toBe("BUZZ")
+    })
+
+    test(`4. Explorer trick equals to "FizzBuzz"`, () => {
+        const explorer = {name: "Explorer2", score: "FIZZBUZZ"}
+        const fbSerive = FizzbuzzService.applyValidationInExplorer(explorer)
+
+        expect(fbSerive.trick).toBe("FIZZBUZZ")
+    })
+})
