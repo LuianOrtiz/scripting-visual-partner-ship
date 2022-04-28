e96c6fb2f56647ad534f28e078d9e863671cfa4b
Luis Ortiz <luiann.ortiz@gmail.com>
2022-04-28 00:23:40 -0500

    Fix testint on BUZZ return

--- test/services/FizzbuzzService.test.js
+++ test/services/FizzbuzzService.test.js
@@ -10,21 +10,21 @@ describe(`Test FizzbuzzService`, () => {
     })
 
     test(`2. Explorer trick equals to "Fizz"`, () => {
-        const explorer = {name: "Explorer2", score: "FIZZ"}
+        const explorer = {name: "Explorer3", score: 3}
         const fbSerive = FizzbuzzService.applyValidationInExplorer(explorer)
 
         expect(fbSerive.trick).toBe("FIZZ")
     })
 
     test(`3. Explorer trick equals to "Buzz"`, () => {
-        const explorer = {name: "Explorer2", score: "BUZZ"}
+        const explorer = {name: "Explorer5", score: 5}
         const fbSerive = FizzbuzzService.applyValidationInExplorer(explorer)
 
         expect(fbSerive.trick).toBe("BUZZ")
     })
 
     test(`4. Explorer trick equals to "FizzBuzz"`, () => {
-        const explorer = {name: "Explorer2", score: "FIZZBUZZ"}
+        const explorer = {name: "Explorer15", score: 15}
         const fbSerive = FizzbuzzService.applyValidationInExplorer(explorer)
 
         expect(fbSerive.trick).toBe("FIZZBUZZ")
